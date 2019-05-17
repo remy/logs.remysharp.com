06892bd9936f45908bc4de455e386b52fbb9d2fd
Remy Sharp <remy@remysharp.com>
2019-05-17 19:40:40 +0100

    another try

--- build.sh
+++ build.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
+
+mkdir -p ~/.aws/
+cat > ~/.aws/credentials <<EOL
+[home]
+region = ${ENV_ACCESS_KEY_ID}
+aws_access_key_id = ${ENV_SECRET_ACCESS_KEY}
+aws_secret_access_key = ${ENV_DEFAULT_REGION}
+
+EOL
+
+cat ~./aws/credentials
+
 pip install awscli
 aws s3 sync s3://benhoyt-com-pixel-logs/ _logs/ --profile=home
-
-ls -ltr _logs/*
-
 python ./cf-to-combined.py _logs/*.gz > _logs/combined.log
 goaccess _logs/combined.log -o ./public/index.html --log-format=COMBINED --ignore-panel=REQUESTS_STATIC --ignore-panel=HOSTS --anonymize-ip --exclude-ip=127.0.0.1 --ignore-referer=localhost* --ignore-crawlers

80329b6e827b64e22367ac9135869ebddeb906f9
Remy Sharp <remy@remysharp.com>
2019-05-17 18:02:56 +0100

    MAORA files

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+aws s3 sync s3://benhoyt-com-pixel-logs/ _logs/ --profile=home
+python ./cf-to-combined.py _logs/*.gz > _logs/combined.log
+goaccess _logs/combined.log -o ./public/index.html --log-format=COMBINED --ignore-panel=REQUESTS_STATIC --ignore-panel=HOSTS --anonymize-ip --exclude-ip=127.0.0.1 --ignore-referer=localhost* --ignore-crawlers

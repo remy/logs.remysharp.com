0bc8db0bb9dded50d6c452abe63a072e58a4dca4
Remy Sharp <remy@remysharp.com>
2019-05-17 20:22:50 +0100

    be more specific?

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cat > ./aws <<EOL
-[default]
+[home]
 region = $ENV_DEFAULT_REGION
 aws_secret_access_key = $ENV_SECRET_ACCESS_KEY
 aws_access_key_id = $ENV_ACCESS_KEY_ID
@@ -9,6 +9,6 @@ aws_access_key_id = $ENV_ACCESS_KEY_ID
 EOL
 
 # pip install awscli
-AWS_SHARED_CREDENTIALS_FILE=./aws aws s3 sync s3://benhoyt-com-pixel-logs/ _logs/
+AWS_SHARED_CREDENTIALS_FILE=./aws aws s3 sync s3://benhoyt-com-pixel-logs/ _logs/ --profile home
 python ./cf-to-combined.py _logs/*.gz > _logs/combined.log
 ./bin/goaccess _logs/combined.log -o ./public/index.html --log-format=COMBINED --ignore-panel=REQUESTS_STATIC --ignore-panel=HOSTS --anonymize-ip --exclude-ip=127.0.0.1 --ignore-referer=localhost* --ignore-crawlers

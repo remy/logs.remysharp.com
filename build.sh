774d87bb426a72d454326683f4ab0f1a5e9b76dd
Remy Sharp <remy@remysharp.com>
2019-05-17 20:01:30 +0100

    another try?

--- build.sh
+++ build.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
-AWS_DEFAULT_REGION=$ENV_DEFAULT_REGION
-AWS_SECRET_ACCESS_KEY=$ENV_SECRET_ACCESS_KEY
-AWS_ACCESS_KEY_ID=$ENV_ACCESS_KEY_ID
+apt-get install goaccess
 
-export AWS_DEFAULT_REGION
-export AWS_SECRET_ACCESS_KEY
-export AWS_ACCESS_KEY_ID
+cat > ~/aws <<EOL
+[default]
+region = $ENV_DEFAULT_REGION
+aws_secret_access_key = $ENV_SECRET_ACCESS_KEY
+aws_access_key_id = $ENV_ACCESS_KEY_ID
 
-
+EOL
 
 # pip install awscli
-aws s3 sync s3://benhoyt-com-pixel-logs/ _logs/
+AWS_SHARED_CREDENTIALS_FILE=./aws aws s3 sync s3://benhoyt-com-pixel-logs/ _logs/
 python ./cf-to-combined.py _logs/*.gz > _logs/combined.log
 goaccess _logs/combined.log -o ./public/index.html --log-format=COMBINED --ignore-panel=REQUESTS_STATIC --ignore-panel=HOSTS --anonymize-ip --exclude-ip=127.0.0.1 --ignore-referer=localhost* --ignore-crawlers

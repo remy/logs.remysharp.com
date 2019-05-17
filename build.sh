c9630815b24eb0cd08c2348956aa939f43ccdbe2
Remy Sharp <remy@remysharp.com>
2019-05-17 22:03:54 +0100

    OMG WHY DID I NOT SPOT IT

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-mkdir ~/.aws
-cat > ~/.aws/credentials <<EOL
-[home]
+# mkdir ~/.aws
+cat > ./aws <<EOL
+[netlify]
 region = $ENV_DEFAULT_REGION
 aws_secret_access_key = $ENV_SECRET_ACCESS_KEY
 aws_access_key_id = $ENV_ACCESS_KEY_ID
@@ -10,7 +10,6 @@ aws_access_key_id = $ENV_ACCESS_KEY_ID
 EOL
 
 # pip install awscli
-# AWS_SHARED_CREDENTIALS_FILE=./aws
-aws s3 sync s3://benhoyt-com-pixel-logs/ _logs/ --profile home
+AWS_SHARED_CREDENTIALS_FILE=./aws aws s3 sync s3://logging.remysharp.com/ _logs/ --profile netlify
 python ./cf-to-combined.py _logs/*.gz > _logs/combined.log
 ./bin/goaccess _logs/combined.log -o ./public/index.html --log-format=COMBINED --ignore-panel=REQUESTS_STATIC --ignore-panel=HOSTS --anonymize-ip --exclude-ip=127.0.0.1 --ignore-referer=localhost* --ignore-crawlers

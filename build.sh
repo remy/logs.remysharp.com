58182199bd41387a3df9fd47c9e6bda158324071
Remy Sharp <remy@remysharp.com>
2019-05-17 20:15:27 +0100

    add binary

--- build.sh
+++ build.sh
@@ -13,4 +13,4 @@ EOL
 # pip install awscli
 AWS_SHARED_CREDENTIALS_FILE=./aws aws s3 sync s3://benhoyt-com-pixel-logs/ _logs/
 python ./cf-to-combined.py _logs/*.gz > _logs/combined.log
-goaccess _logs/combined.log -o ./public/index.html --log-format=COMBINED --ignore-panel=REQUESTS_STATIC --ignore-panel=HOSTS --anonymize-ip --exclude-ip=127.0.0.1 --ignore-referer=localhost* --ignore-crawlers
+./bin/goaccess _logs/combined.log -o ./public/index.html --log-format=COMBINED --ignore-panel=REQUESTS_STATIC --ignore-panel=HOSTS --anonymize-ip --exclude-ip=127.0.0.1 --ignore-referer=localhost* --ignore-crawlers

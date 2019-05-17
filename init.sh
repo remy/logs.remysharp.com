2a33184af3b4ec20b51443dcbc29ca0b4c9bca2a
Remy Sharp <remy@remysharp.com>
2019-05-17 22:47:14 +0100

    clean up

--- init.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-sed -i s/ENV_ACCESS_KEY_ID/${ENV_ACCESS_KEY_ID}/g netlify.toml
-sed -i s/ENV_SECRET_ACCESS_KEY/${ENV_SECRET_ACCESS_KEY}/g netlify.toml
-sed -i s/ENV_DEFAULT_REGION/${ENV_DEFAULT_REGION}/g netlify.toml
-

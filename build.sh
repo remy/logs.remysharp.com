27103641897d3c4ccb7b05fed165912b9e5f221a
Remy Sharp <remy@remysharp.com>
2019-07-13 14:09:15 +0100

    try to be smart about timeframe

--- build.sh
+++ build.sh
@@ -9,7 +9,18 @@ aws_access_key_id = $ENV_ACCESS_KEY_ID
 
 EOL
 
+INCLUDE="";
+for i in $(seq 1 31); do
+  INCLUDE="$INCLUDE --include \"*.$(date --date="-$i days" "+%Y-%m-%d")-*\"";
+done
+
 # pip install awscli
-AWS_SHARED_CREDENTIALS_FILE=./aws aws s3 sync s3://logging.leftlogic.com/ . --profile netlify
+echo $INCLUDE;
+AWS_SHARED_CREDENTIALS_FILE=./aws aws s3 sync s3://logging.leftlogic.com/ . --profile netlify --exclude "*" $INCLUDE
+
+
+# aws s3 sync s3://logging.leftlogic.com/ . --profile leftlogic --exclude "*" $INCLUDE
+
 python ./cf-to-combined.py ./logs/*.gz > ./logs/combined.log
+
 ./bin/goaccess ./logs/combined.log -o ./public/index.html --log-format=COMBINED --ignore-panel=REQUESTS_STATIC --ignore-panel=HOSTS --anonymize-ip --exclude-ip=127.0.0.1 --ignore-referer=localhost* --ignore-crawlers --ignore-panel=STATUS_CODES --ignore-panel=NOT_FOUND
